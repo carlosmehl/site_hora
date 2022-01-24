4e20fe36b99acbe7a5dc43c379b6424023e9b627
Carlos Mehl <carlosmehlcontato@gmail.com>
2022-01-23 23:00:52 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+function carregar(){
+var hora = window.document.getElementById('hora')
+var img = window.document.getElementById('imagem')
 var agora = new Date()
-        var hr = agora.getHours()
-        var min = agora.getMinutes()
-        hora.innerHTML = `Agora são ${hr} horas e ${min} minutos`
+// var hr = agora.getHours()
+var min = agora.getMinutes()
+var hr = 19
+    hora.innerHTML = `Agora são ${hr} horas e ${min} minutos`
+if (hr >= 06 && hr < 12){
+    img.src = 'imagens/manha_peq.jpg'
+    document.body.style.background = '#919cb2'
+    document.body.style.color = 'black'
+}else if(hr >= 12 && hr <= 18){
+    img.src = 'imagens/tarde_peq.jpg'
+    document.body.style.background = '#bba695'
+}else{
+    img.src = 'imagens/noite_peq.jpg'
+    document.body.style.background = '#08526d'
+}
+}
